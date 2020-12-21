b1e328bfeb70c3d07c6facf8da2d26ffab5f6cef
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2020-12-21 18:58:06 -0300

    Se actualiza gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -7,39 +7,89 @@
 def call(){
     def stages_list = ['build', 'test', 'jar', 'sonar', 'run', 'testing', 'nexus']
 
-    println "Stages a ejecutar {$params.STAGE}"
-    
-    stage('build'){
-        env.STG_NAME = 'build'
-        bat 'gradle clean build'
+    def sStages = params.STAGE
+
+    if(sStages.trim() == ''){
+        println "Stages a ejecutar [TODOS]"
+    }else{
+        println "Stages a ejecutar [{$sStages}]"
     }
-    stage('test'){
-        env.STG_NAME = 'test'
-        bat 'gradle test'
-    }
-    stage('jar'){
-        env.STG_NAME = 'jar'
-        bat 'gradle jar'
-    }
-    stage('sonar'){
-        env.STG_NAME = 'sonar'
-        def scannerHome = tool 'sonar-scanner';
-        withSonarQubeEnv('sonar') {
-            bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build -Dsonar.login=75a0e9b0613f563c0e69a23174cf79eb5d4d74c7"
+
+    def str = sStages.split(';');
+
+    if(sStages.trim() != ''){
+        for(String values : str){
+            if (!stages_list.contains(values.trim())){
+                env.STG_NAME = "Stage no valido: ${values}"
+                error "Stage no valido"
+            }
         }
     }
-    stage('run'){
-        env.STG_NAME = 'run'
-        bat 'start gradle bootRun'
-    }
-    stage('testing'){
-        env.STG_NAME = 'testing'
-        sleep 20
-        bat 'curl http://localhost:8082/rest/mscovid/estadoMundial'
-    }
-    stage('nexus'){
-        env.STG_NAME = 'nexus'
-        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
+
+    for(String values : stages_list){
+
+        switch(values){
+            case 'build':
+                if(sStages.trim() == '' || str.contains(values)){
+                    stage('build'){
+                        env.STG_NAME = 'build'
+                        bat 'gradle clean build'
+                    }
+                }
+                break
+            case 'test':
+                if(sStages.trim() == '' || str.contains(values)){
+                    stage('test'){
+                        env.STG_NAME = 'test'
+                        bat 'gradle test'
+                    }
+                }
+                break
+            case 'jar':
+                if(sStages.trim() == '' || str.contains(values)){
+                    stage('jar') {
+                        env.STG_NAME = 'Jar'
+                        bat 'gradle jar'
+                    }
+                }
+                break
+            case 'sonar':
+                if(sStages.trim() == '' || str.contains(values)){
+                    stage('sonar'){
+                        env.STG_NAME = 'sonar'
+                        def scannerHome = tool 'sonar-scanner';
+                        withSonarQubeEnv('sonar') {
+                            bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build -Dsonar.login=75a0e9b0613f563c0e69a23174cf79eb5d4d74c7"
+                        }
+                    }
+                }
+                break 
+            case 'run':
+                if(sStages.trim() == '' || str.contains(values)){
+                    stage('run'){
+                        env.STG_NAME = 'run'
+                        bat 'start gradle bootRun'
+                    }
+                }
+                break
+            case 'testing':
+                if(sStages.trim() == '' || str.contains(values)){
+                    stage('testing'){
+                        env.STG_NAME = 'testing'
+                        sleep 20
+                        bat 'curl http://localhost:8082/rest/mscovid/estadoMundial'
+                    }
+                }
+                break
+            case 'nexus':
+                if(sStages.trim() == '' || str.contains(values)){
+                    stage('nexus'){
+                        env.STG_NAME = 'nexus'
+                        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'build\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
+                    }
+                }
+                break
+        }
     }
 
 }

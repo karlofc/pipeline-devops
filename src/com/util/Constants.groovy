812d02f64770e8d4feb3c7825b482573000ddbbd
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2021-01-06 17:50:40 -0300

    Se actualiza ejecucion.groovy

--- src/com/util/Constants.groovy
+++ src/com/util/Constants.groovy
@@ -8,6 +8,7 @@ class Constants {
     public static final STAGES = [STAGE_BUILD, STAGE_TEST, STAGE_JAR, STAGE_SONAR, STAGE_RUN, STAGE_TESTING, STAGE_NEXUS]
     public static final STAGES_MAVEN = [STAGE_BUILD, STAGE_TEST, STAGE_JAR, STAGE_SONAR, STAGE_RUNJAR, STAGE_REST, STAGE_NEXUSCI]
     public static final STAGES_GRADLE = [STAGE_BUILDANDTEST, STAGE_SONAR, STAGE_RUNJAR, STAGE_REST, STAGE_NEXUSCI]
+    public static final STAGES_RELEASE = [STAGE_DOWNLOADNEXUS, STAGE_RUNDOWNLOADEDNEXUS, STAGE_REST, STAGE_NEXUSCD]
     public static final String SPLIT_SYMBOL = ';'
     public static final String STAGE_BUILD = 'build'
     public static final String STAGE_BUILDANDTEST = 'buildAndTest'
@@ -20,4 +21,7 @@ class Constants {
     public static final String STAGE_NEXUS = 'nexus'
     public static final String STAGE_REST = 'rest'
     public static final String STAGE_NEXUSCI = 'nexusCI'
+    public static final String STAGE_DOWNLOADNEXUS = 'downloadNexus'
+    public static final String STAGE_RUNDOWNLOADEDNEXUS = 'runDownloadedNexus'
+    public static final String STAGE_NEXUSCD = 'nexusCD'
 }

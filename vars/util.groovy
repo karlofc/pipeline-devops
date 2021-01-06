30b65d7477daf7db73026aa2b78bf68f62c1cedc
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2021-01-06 18:16:00 -0300

    Se actualiza ejecucion.groovy

--- vars/util.groovy
+++ vars/util.groovy
@@ -39,7 +39,10 @@ def pipelineType(branch_name){
     if(branch_name ==~ /develop/ || branch_name ==~ /feature-.*/){
         pipeline_type = Constants.IC
         figlet "continuous integration"
-    } else {
+    } else if(branch_name ==~ /release-v\d{1,}-\d{1,}-\d{1,}$/){
+        pipeline_type = Constants.RELEASE
+        figlet "continuous deployment"
+    } else if(branch_name ==~ /origin\/release-v\d{1,}-\d{1,}-\d{1,}$/){
         pipeline_type = Constants.RELEASE
         figlet "continuous deployment"
     }
